66e6e6c0945b438907ce8d865be868b53749f402
Gus Prado <gusgprado@outlook.com>
2019-07-19 13:40:47 -0300

    Validation added
    CRUD with MongoDB

--- controllers/series.js
+++ controllers/series.js
@@ -23,11 +23,11 @@ const novaProcess = async({ Serie }, req, res) => {
 }
 
 const novaForm = (req, res) => {
-    res.render('series/nova')
+    res.render('series/nova', { errors: [] })
 }
 
 const excluir = async({ Serie }, req, res) => {
-    await Serie.remove({ _id: req.params.id })
+    await Serie.deleteOne({ _id: req.params.id })
     res.redirect('/series')
 }
 
@@ -35,19 +35,36 @@ const editarProcess = async({ Serie }, req, res) => {
     const serie = await Serie.findOne({ _id: req.params.id })
     serie.name = req.body.name
     serie.status = req.body.status
-    await serie.save()
-    res.redirect('/series')
+    try {
+        await serie.save()
+        res.redirect('/series')
+    } catch (e) {
+        res.render('series/editar', { serie, labels, errors: Object.keys(e.errors) })
+    }
 }
 
 const editarForm = async({ Serie }, req, res) => {
     const serie = await Serie.findOne({ _id: req.params.id })
-    res.render('series/editar', { serie, labels })
+    res.render('series/editar', { serie, labels, errors: [] })
 }
+
+const info = async({ Serie }, req, res) => {
+    const serie = await Serie.findOne({ _id: req.params.id })
+    res.render('series/info', { serie })
+}
+
+const addComment = async({ Serie }, req, res) => {
+    await Serie.updateOne({ _id: req.params.id }, { $push: { comments: req.body.comentario } })
+    res.redirect('/series/info/' + req.params.id)
+}
+
 module.exports = {
     index,
     novaProcess,
     novaForm,
     excluir,
     editarForm,
-    editarProcess
+    editarProcess,
+    info,
+    addComment
 }
